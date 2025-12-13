1952c7eca4b55fd04e84e656d7d283796a388568
acd <acdch@galleon.com>
2025-12-13 11:31:15 +0100

    player test

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,41 @@ int screenHeight = 450;
 
 raylib::Window window(screenWidth, screenHeight, "Project: Neutronic Decay");
 
+class Player {
+    public:
+
+    raylib::Vector2 position;
+    float radius;
+    float speed;
+    bool isMoving;
+
+    Player() {
+        position = raylib::Vector2(0, 0);
+        radius = 20;
+        isMoving = false;
+        speed = 5;
+    }
+
+    void update(float deltaTime) {
+        handleInputs(deltaTime);
+    }
+
+    void draw() {
+        DrawCircle(position.x, position.y, radius, DARKGREEN);
+    }
+
+    void handleInputs(float deltaTime) {
+        isMoving = false;
+        if (IsKeyDown(KEY_D)) { position.x += speed; isMoving = true;}  // Right
+        if (IsKeyDown(KEY_A)) { position.x += -speed; isMoving = true;} // Left
+        if (IsKeyDown(KEY_W)) { position.y += -speed; isMoving = true;} // Up
+        if (IsKeyDown(KEY_S)) { position.y += speed; isMoving = true;}  // Down
+    }
+
+};
+
+Player player;
+
 int main() {
 
 
@@ -23,15 +58,7 @@ int main() {
 
     while (!window.ShouldClose())
     {
-        BeginDrawing();
-
-        window.ClearBackground(RAYWHITE);
-
-        DrawText("Congrats! You created your first window!", 190, 200, 20, GOLD);
-
-        // Object methods.
-
-        EndDrawing();
+        UpdateDrawFrame();
     }
 
     #endif
@@ -42,12 +69,15 @@ int main() {
 }
 
 void UpdateDrawFrame() {
+
+    player.update(GetFrameTime());
+
     BeginDrawing();
 
     window.ClearBackground(RAYWHITE);
 
     DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
+    player.draw();
     // Object methods.
 
     EndDrawing();
