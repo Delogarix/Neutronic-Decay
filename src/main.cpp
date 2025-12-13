a0da63ab1020689e9ad91be488148271add94653
acd <acdch@galleon.com>
2025-12-13 11:15:05 +0100

    web func definition

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,12 @@
     #include <emscripten/emscripten.h>
 #endif
 
+void UpdateDrawFrame();
+
 int screenWidth = 800;
 int screenHeight = 450;
 
-raylib::Window window(screenWidth, screenHeight, "raylib-cpp - basic window");
+raylib::Window window(screenWidth, screenHeight, "Project: Neutronic Decay");
 
 int main() {
 
@@ -25,7 +27,7 @@ int main() {
 
         window.ClearBackground(RAYWHITE);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, GOLD);
 
         // Object methods.
 
